8487ce2d9b559d93c2908c3cf88c348dff84e20f
jan.tampos <jan.tampos@wizeline.com>
2019-08-08 15:16:42 +0700

    /tictactoe-app props from Board component

--- tictactoe-app/src/index.js
+++ tictactoe-app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-/*
+/* Square as Class Component
 class Square extends React.Component {
   render() {
     return (
