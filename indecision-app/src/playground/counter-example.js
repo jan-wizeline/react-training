0cd7a09c36065b33528c753a35a683271fafefd3
jan.tampos <jan.tampos@wizeline.com>
2019-08-16 15:05:39 +0700

    33. Adding State to Counter App; Part II

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -1,27 +1,43 @@
 class Counter extends React.Component {
   constructor(props) {
-    super(props);
-    this.handleAddOne = this.handleAddOne.bind(this);
-    this.handleMinusOne = this.handleMinusOne.bind(this);
-    this.handleReset = this.handleReset.bind(this);
+    super(props)
+    this.handleAddOne = this.handleAddOne.bind(this)
+    this.handleMinusOne = this.handleMinusOne.bind(this)
+    this.handleReset = this.handleReset.bind (this)
+
+    this.state = {
+      count: 0
+    }
   }
 
   handleAddOne() {
-    console.log('addOne');
+    this.setState((prevState) => {
+      return {
+        count: prevState.count  + 1
+      }
+    })
   }
 
   handleMinusOne() {
-    console.log('minusOne');
+    this.setState((prevState) => {
+      return {
+        count: prevState.count  - 1
+      }
+    })
   }
 
   handleReset() {
-    console.log('reset');
+   this.setState(() => {
+     return {
+       count: 0
+     }
+   })
   }
 
   render() {
     return (
       <div>
-        <h1>Count: 123</h1>
+        <h1>Count: {this.state.count}</h1>
         <button onClick={this.handleAddOne}> +1 </button>
         <button onClick={this.handleMinusOne}> -1 </button>
         <button onClick={this.handleReset}>reset</button>
