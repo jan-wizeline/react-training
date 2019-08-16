c6d4548fd41498ad7baedda4d6a883f650825373
jan.tampos <jan.tampos@wizeline.com>
2019-08-16 12:16:20 +0700

    26.Creating a React Component

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -15,6 +15,42 @@ class Person {
   }
 }
 
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+      description += ` His/her major is ${this.major}`;
+    }
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name,age,homeLocation) {
+    super(name,age);
+    this.homeLocation = homeLocation;
+  }
+  hasHomeLocation() {
+    return !!this.homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+    if (this.hasHomeLocation()) {
+      greeting += ` I'm visiting from ${this.homeLocation}`
+    }
+    return greeting;
+  }
+}
+
+
 const me = new Person('Jan Tampos', 25);
 console.log(me);
 console.log(me.getGreeting());
@@ -26,3 +62,23 @@ console.log(other);
 console.log(other.getGreeting());
 console.log(other.getDescription());
 
+const meStudent = new Student('Jan Tampos', 25, 'Computer Science');
+console.log(meStudent);
+console.log(meStudent.hasMajor());
+console.log(meStudent.getDescription());
+
+
+const otherStudent = new Student();
+console.log(otherStudent);
+console.log(otherStudent.hasMajor());
+console.log(otherStudent.getDescription());
+
+
+const meTraveler = new Traveler('Jan Tampos', 25, 'Philippines');
+console.log(meTraveler);
+console.log(meTraveler.getGreeting());
+
+
+const otherTraveler = new Traveler();
+console.log(otherTraveler);
+console.log(otherTraveler.getGreeting());
